cdf19125f8aecfa9fa9914a0c367d693ea215ab2
carnivalcompanion <carnivalcompanion@gmail.com>
2025-12-27 13:28:48 -0500

    Update cloud_storage.py

--- modules/cloud_storage.py
+++ modules/cloud_storage.py
@@ -1,7 +1,8 @@
 import os
+import json
+from oauth2client.service_account import ServiceAccountCredentials
 from pydrive2.auth import GoogleAuth
 from pydrive2.drive import GoogleDrive
-from oauth2client.service_account import ServiceAccountCredentials
 
 class CloudStorage:
     def __init__(self):
@@ -9,20 +10,19 @@ class CloudStorage:
 
     def _authenticate(self):
         scopes = ["https://www.googleapis.com/auth/drive"]
-        key_file = os.getenv("GOOGLE_SERVICE_ACCOUNT", "service_account.json")
-        credentials = ServiceAccountCredentials.from_json_keyfile_name(key_file, scopes)
+
+        sa_json = os.getenv("GOOGLE_SERVICE_ACCOUNT")
+        if not sa_json:
+            raise RuntimeError("❌ GOOGLE_SERVICE_ACCOUNT env var missing")
+
+        credentials_dict = json.loads(sa_json)
+
+        credentials = ServiceAccountCredentials.from_json_keyfile_dict(
+            credentials_dict,
+            scopes
+        )
+
         gauth = GoogleAuth()
         gauth.credentials = credentials
-        return GoogleDrive(gauth)
 
-    def upload_media(self, local_path):
-        folder_id = os.getenv("PUBLIC_DRIVE_FOLDER_ID")
-        filename = os.path.basename(local_path)
-        file = self.drive.CreateFile({
-            "title": filename,
-            "parents": [{"id": folder_id}]
-        })
-        file.SetContentFile(local_path)
-        file.Upload()
-        file.InsertPermission({"type": "anyone", "value": "anyone", "role": "reader"})
-        return f"https://drive.google.com/uc?export=download&id={file['id']}"
+        return GoogleDrive(gauth)
