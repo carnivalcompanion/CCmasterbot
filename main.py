1c02653c0848dd910ae9fc777e3c2046b8ee523c
carnivalcompanion <carnivalcompanion@gmail.com>
2025-12-30 17:36:51 -0500

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import os
 import threading
 import time
 import logging
-from video_processor import VideoProcessor
+from videoprocessor import VideoProcessor
 
 # Configure logging
 logging.basicConfig(level=logging.INFO)
